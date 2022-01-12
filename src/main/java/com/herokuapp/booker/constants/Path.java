7335e6fdeef284b16e7cf1ea3c5e463f82f9f6a8
primetraineesix <primetraineesix@gmail.com>
2022-01-12 18:20:32 +0000

    restful-booker

---
+++ src/main/java/com/herokuapp/booker/constants/Path.java
@@ -0,0 +1,19 @@
+package com.herokuapp.booker.constants;
+
+/**
+ * Created by Jay
+ */
+public class Path {
+    /*
+     * This is base path for student api
+     */
+    public static final String STUDENT = "/student";
+
+
+    /*
+     * This is base path for authenticate api
+     */
+    public static final String AUTHENTICATE = "/authenticate";
+
+
+}
---
+++ src/main/java/com/herokuapp/booker/params/Headers.java
@@ -0,0 +1,22 @@
+package com.herokuapp.booker.params;
+
+import java.util.HashMap;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Headers {
+
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String ACCEPT = "Accept";
+    public static final String COOKIE = "Cookie";
+
+
+    public static HashMap<String, String> getHeaders(String token) {
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put(CONTENT_TYPE, "application/json");
+        headers.put(ACCEPT, "application/json");
+        headers.put(COOKIE, "token=" + token);
+        return headers;
+    }
+}
